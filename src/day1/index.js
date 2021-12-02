bd9c49bce729c7127177e6f2f623c1a5f28928a7
Yasirah Krueng <ykrueng@gmail.com>
2021-12-01 20:05:06 -0600

    day1 - one liner

--- src/day1/index.js
+++ src/day1/index.js
@@ -1,21 +1,3 @@
-exports.sonarSweep = function (arr) {
-  let count = 0
-  for (let i=1; i<arr.length; i++) {
-    if (arr[i] > arr[i-1]) {
-      count++
-    }
-  }
-  return count
-}
+exports.sonarSweep = a => a.reduce((p, c, i) => i === 0 ? 0 : c > a[i-1] ? ++p : p, 0)
 
-exports.sonarSweepSliding = function (arr) {
-  let count = 0
-  let prev = arr[0] + arr[1] + arr[2]
-
-  for (let i=1; i<arr.length-2; i++) {
-    const current = arr[i] + arr[i+1] + arr[i+2]
-    if (current > prev) count++
-    prev = current
-  }
-  return count
-}
+exports.sonarSweepSliding = a => a.reduce((p, c, i) => i < 3 ? 0 : c > a[i-3] ? ++p : p)
